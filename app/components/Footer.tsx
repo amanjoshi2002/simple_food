45745da645fd997ecc258156f9f1fcbca9492043
amanjoshi2002 <sctaman21@gmail.com>
2024-07-14 22:16:29 +0530

    updated footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,42 +3,42 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer text-base-content p-10 bg-yellow-400">
-    <nav>
-      <h6 className="footer-title">Services</h6>
-      <a className="link link-hover">Branding</a>
-      <a className="link link-hover">Design</a>
-      <a className="link link-hover">Marketing</a>
-      <a className="link link-hover">Advertisement</a>
-    </nav>
-    <nav>
-      <h6 className="footer-title">Company</h6>
-      <a className="link link-hover">About us</a>
-      <a className="link link-hover">Contact</a>
-      <a className="link link-hover">Jobs</a>
-      <a className="link link-hover">Press kit</a>
-    </nav>
-    <nav>
-      <h6 className="footer-title">Legal</h6>
-      <a className="link link-hover">Terms of use</a>
-      <a className="link link-hover">Privacy policy</a>
-      <a className="link link-hover">Cookie policy</a>
-    </nav>
-    <form>
-      <h6 className="footer-title">Newsletter</h6>
-      <fieldset className="form-control w-80">
-        <label className="label">
-          <span className="label-text">Enter your email address</span>
-        </label>
-        <div className="join">
-          <input
-            type="text"
-            placeholder="username@site.com"
-            className="input input-bordered join-item" />
-          <button className="btn bg bg-white join-item text-yellow-400">Subscribe</button>
-        </div>
-      </fieldset>
-    </form>
-  </footer>
+      <nav>
+        <h6 className="footer-title">Explore</h6>
+        <a className="link link-hover">Recipes</a>
+        <a className="link link-hover">Healthy Eating</a>
+        <a className="link link-hover">Cooking Tips</a>
+        <a className="link link-hover">Meal Plans</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">About Simple Food</h6>
+        <a className="link link-hover">Our Story</a>
+        <a className="link link-hover">Contact Us</a>
+        <a className="link link-hover">Join Our Team</a>
+        <a className="link link-hover">Media Kit</a>
+      </nav>
+      <nav>
+        <h6 className="footer-title">Resources</h6>
+        <a className="link link-hover">Terms of Service</a>
+        <a className="link link-hover">Privacy Policy</a>
+        <a className="link link-hover">Cookie Policy</a>
+      </nav>
+      <form>
+        <h6 className="footer-title">Subscribe to Our Newsletter</h6>
+        <fieldset className="form-control w-80">
+          <label className="label">
+            <span className="label-text">Enter your email address</span>
+          </label>
+          <div className="join">
+            <input
+              type="email"
+              placeholder="username@site.com"
+              className="input input-bordered join-item" />
+            <button className="btn bg bg-white join-item text-yellow-400">Subscribe</button>
+          </div>
+        </fieldset>
+      </form>
+    </footer>
   );
 };
 
